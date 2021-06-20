554e99485159505e660a82bb88072e1a5eea1c18
hoangdesu <hoangdesu@gmail.com>
2021-06-20 16:00:03 +0700

    update styles

--- FilterListVanillaJS/main.js
+++ FilterListVanillaJS/main.js
@@ -10,11 +10,14 @@ const filterNames = () => {
         let anchor = item.getElementsByTagName('a')[0];
         let name = anchor.innerHTML.toLowerCase();
         // console.log(name);
+    
         if (name.indexOf(value) > -1) {
             item.classList.remove('is-hidden');
         } else {
             item.classList.add('is-hidden');
+            // item.classList.add('has-background-white');
         }
+
     })
 }
 

51c286f93a77efa9f4789a4cc3c47a6b247109e8
hoangdesu <hoangdesu@gmail.com>
2021-06-20 15:18:25 +0700

    Filter Vanilla JS

---
+++ FilterListVanillaJS/main.js
@@ -0,0 +1,21 @@
+let search = document.querySelector('#search');
+let items = document.querySelectorAll('.item');
+
+// console.log(items);
+
+const filterNames = () => {
+    let value = search.value.toLowerCase();
+    // console.log(value);
+    items.forEach(item => {
+        let anchor = item.getElementsByTagName('a')[0];
+        let name = anchor.innerHTML.toLowerCase();
+        // console.log(name);
+        if (name.indexOf(value) > -1) {
+            item.classList.remove('is-hidden');
+        } else {
+            item.classList.add('is-hidden');
+        }
+    })
+}
+
+search.addEventListener('keyup', filterNames)
